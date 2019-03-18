b1bab1b0a3d6fd49c2c6e4a638c202baaeb837c5
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-03-18 14:30:45 +0100

    Implement host emulation in CLI main()

--- dqcsim-cli/src/main.rs
+++ dqcsim-cli/src/main.rs
@@ -1,4 +1,6 @@
+use dqcsim::reproduction::HostCall;
 use dqcsim::simulator::Simulator;
+use dqcsim::*;
 use failure::Error;
 
 mod arg_parse;
@@ -9,7 +11,53 @@ fn main() -> Result<(), Error> {
         println!("{}", e);
         std::process::exit(1);
     });
-    dbg!(&cfg);
-    Simulator::try_from(cfg.dqcsim)?.as_mut().abort(true)?;
+
+    let mut sim = Simulator::try_from(cfg.dqcsim).unwrap_or_else(|e| {
+        eprintln!("Failed to construct simulator: {}", e);
+        std::process::exit(1);
+    });
+
+    for host_call in cfg.host_calls.into_iter() {
+        match host_call {
+            HostCall::Start(d) => {
+                info!("Executing 'start(...)' host call...");
+                sim.as_mut().start(d)?;
+            }
+            HostCall::Wait => {
+                info!("Executing 'wait()' host call...");
+                let ret = sim.as_mut().wait()?;
+                note!("'wait()' returned {}", &ret);
+                if cfg.host_stdout {
+                    println!("wait(): {}", ret);
+                }
+            }
+            HostCall::Send(d) => {
+                info!("Executing 'send(...)' host call...");
+                sim.as_mut().send(d)?;
+            }
+            HostCall::Recv => {
+                info!("Executing 'recv()' host call...");
+                let ret = sim.as_mut().recv()?;
+                note!("'recv()' returned {}", &ret);
+                if cfg.host_stdout {
+                    println!("recv: {}", ret);
+                }
+            }
+            HostCall::Yield => {
+                info!("Executing 'yield()' host call...");
+                sim.as_mut().yield_to_frontend()?;
+            }
+            HostCall::Arb(n, d) => {
+                info!("Executing 'arb(...)' host call...");
+                let ret = sim.as_mut().arb(n, d)?;
+                note!("'arb()' returned {}", &ret);
+                if cfg.host_stdout {
+                    println!("arb: {}", ret);
+                }
+            }
+        }
+    }
+
+    sim.as_mut().abort(true)?;
     Ok(())
 }
