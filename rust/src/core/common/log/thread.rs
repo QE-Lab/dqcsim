QE-Lab/dqcsim

commit 88885c6d50b5f35565fbb003a08cd4e21745c632

Prevent LogThread panic if stderr can't be wrapped. (#78)
Prevent LogThread panic if stderr can't be wrapped

rust/src/core/common/log/thread.rs
@@ -2,7 +2,7 @@
 
 use crate::{
     common::{
-        error::{err, oe_log_err, Result},
+        error::{err, Result},
         log::{
             callback::LogCallback,
             deinit, init,
@@ -48,7 +48,8 @@ impl LogThread {
         // Spawn the local channel log thread.
         let handler = thread::spawn(move || {
             let mut t = if stderr_level > LoglevelFilter::Off {
-                Some(stderr().ok_or_else(oe_log_err("Failed to acquire stderr"))?)
+                // This may return None which results in no logging to stderr
+                stderr()
             } else {
                 None
             };

commit 04168fe89954bb172f6faa64ff8120430d1e2c1b

Prevent panic in LogThread introduced by #78

rust/src/core/common/log/thread.rs
@@ -60,8 +60,8 @@ impl LogThread {
                     .unwrap()
                     .supports_attr(term::Attr::ForegroundColor(9));
 
-            let trace = stderr_level >= LoglevelFilter::Trace;
-            let debug = stderr_level >= LoglevelFilter::Debug;
+            let trace = t.is_some() && stderr_level >= LoglevelFilter::Trace;
+            let debug = t.is_some() && stderr_level >= LoglevelFilter::Debug;
 
             let tee_files: Vec<TeeFile> = tee_files
                 .into_iter()
@@ -84,7 +84,7 @@ impl LogThread {
                     .for_each(|tf| tf.log(&record));
 
                 // Standard Error
-                if level <= stderr_level {
+                if t.is_some() && level <= stderr_level {
                     let t = t.as_mut().unwrap();
                     let color: term::color::Color = record.level().into();
 