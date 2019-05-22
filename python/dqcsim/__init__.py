941a011930b6a782ca545825fb5aeba5285ee86f
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-05-22 17:14:23 +0200

    Remove dqcsim.tests from pdoc3 docs after failure

--- python/dqcsim/__init__.py
+++ python/dqcsim/__init__.py
@@ -5,6 +5,9 @@ This library allows you to write DQCsim host programs and plugins in Python.
 
 __all__ = ['raw', 'common', 'plugin', 'host']
 
+# Don't try to output documentation for the test module.
+__pdoc__ = {'tests': False}
+
 import dqcsim._dqcsim as raw
 from dqcsim import common
 from dqcsim import plugin
