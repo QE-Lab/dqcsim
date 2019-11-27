2a4b84951e5b2b63b154b973b0606f6f62a12cf7
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-11-27 18:26:26 +0100

    Use virtual inheritance...
    ... to separate constructor methods for a specific handle type from the
    generic accessors that multiple handle types support.

--- cpp/test/wrap/arb.cpp
+++ cpp/test/wrap/arb.cpp
@@ -6,7 +6,7 @@ using namespace dqcsim;
 
 // Test JSON access by means of JSON strings.
 TEST(arb, json) {
-  wrap::Arb data;
+  wrap::ArbData data;
   EXPECT_EQ(data.get_json(), "{}");
   data.set_json("{\"hello\": \"world\"}");
   EXPECT_ERROR(data.set_json("invalid JSON"), "Invalid argument: expected value at line 1 column 1");
@@ -16,7 +16,7 @@ TEST(arb, json) {
 
 // Test JSON access by means of CBOR objects.
 TEST(arb, cbor) {
-  wrap::Arb data;
+  wrap::ArbData data;
   EXPECT_EQ(data.get_cbor(), "\xBF\xFF");
   data.set_cbor("\xBF\x65\x68\x65\x6C\x6C\x6F\x65\x77\x6F\x72\x6C\x64\xFF");
   EXPECT_ERROR(data.set_cbor("\xFF"), "Invalid argument: unexpected code at offset 1");
@@ -27,7 +27,7 @@ TEST(arb, cbor) {
 // Tests the unstructured string list accessors, as well as the copy assignment
 // operator and copy constructor.
 TEST(arb, unstructured) {
-  wrap::Arb data;
+  wrap::ArbData data;
   EXPECT_ERROR(data.get_string(0), "Invalid argument: index out of range: 0");
   data.push_string("hello");
   data.push_string("world");
@@ -47,7 +47,7 @@ TEST(arb, unstructured) {
   EXPECT_EQ(data.get_string(2), "C");
   EXPECT_ERROR(data.get_string(3), "Invalid argument: index out of range: 3");
 
-  wrap::Arb data2(data);
+  wrap::ArbData data2(data);
   data.clear_strings();
   EXPECT_EQ(data.get_string_count(), 0);
   data = data2;
