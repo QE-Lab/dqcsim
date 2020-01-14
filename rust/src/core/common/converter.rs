c9b35cdacfd6b8b56ad4f5291be99d8e45a528b2
Jeroen van Straten <jeroen.van.straten@gmail.com>
2020-01-14 13:46:44 +0100

    WIP

--- rust/src/core/common/converter.rs
+++ rust/src/core/common/converter.rs
@@ -6,8 +6,27 @@
 //! [`Converter`]: ./trait.Converter.html
 //! [`ConverterMap`]: ./struct.ConverterMap.html
 
-use crate::common::error::{oe_err, oe_inv_arg, Result};
+use crate::common::{
+    error::{inv_arg, oe_err, oe_inv_arg, Result},
+    types::{ArbData, Gate, Matrix, QubitRef},
+};
 use std::{cell::RefCell, collections::HashMap, hash::Hash};
+
+/// A type that can be constructed from (part of) an ArbData object.
+pub trait FromArb
+where
+    Self: std::marker::Sized,
+{
+    /// Construct from the given ArbData, taking the parameters used for
+    /// construction out of the data object.
+    fn from_arb(src: &mut ArbData) -> Result<Self>;
+}
+
+/// A type that can be converted into an ArbData object.
+pub trait ToArb {
+    /// Convert self to ArbData parameters and add them to the data object.
+    fn to_arb(self, dest: &mut ArbData);
+}
 
 /// A type that can be used as a Converter.
 ///
@@ -20,18 +39,119 @@ use std::{cell::RefCell, collections::HashMap, hash::Hash};
 /// to convert common types to plugin-specific types and back. This is
 /// primarily used for the C API, where the user cannot define their own
 /// converter traits to do the equivalent more ergonomically.
-pub trait Converter<I, O> {
+pub trait Converter {
+    /// The more generic detector input type = constructor output type.
+    type Input;
+    /// The more specific detector output type = constructor input type.
+    type Output;
     /// The detect function implements the detector function. The return values
     /// are as follows:
     ///  - `Ok(Some(O))`: successful match
     ///  - `Ok(None)`: the input is not an instance of the output type
     ///  - `Err(_)`: something went wrong during detection
-    fn detect(&self, input: &I) -> Result<Option<O>>;
+    fn detect(&self, input: &Self::Input) -> Result<Option<Self::Output>>;
     /// The construct function implements the opposite of the detector
     /// function, converting the plugin-specific type to the more generic type.
     ///  - `Ok(O)`: successful construction
     ///  - `Err(_)`: something went wrong during construction
-    fn construct(&self, input: &O) -> Result<I>;
+    fn construct(&self, input: &Self::Output) -> Result<Self::Input>;
+}
+
+/// Converter implementation for unitary gates based on a matrix converter.
+pub struct UnitaryGateConverter<M>
+where
+    M: Converter<Input = Matrix>,
+{
+    matrix_converter: M,
+    num_controls: Option<usize>,
+}
+
+impl<M> UnitaryGateConverter<M>
+where
+    M: Converter<Input = Matrix>,
+{
+    pub fn new(matrix_converter: M, num_controls: Option<usize>) -> Self {
+        Self {
+            matrix_converter,
+            num_controls,
+        }
+    }
+}
+
+impl<M> Converter for UnitaryGateConverter<M>
+where
+    M: Converter<Input = Matrix>,
+    M::Output: FromArb + ToArb,
+{
+    type Input = Gate;
+    type Output = (Vec<QubitRef>, ArbData);
+
+    fn detect(&self, gate: &Gate) -> Result<Option<Self::Output>> {
+        if gate.get_name().is_some() || !gate.get_measures().is_empty() {
+            // Custom gate, measurement gate or unitary + measure compound; not
+            // (just) a unitary so no match.
+            Ok(None)
+        } else if let Some(matrix) = gate.get_matrix() {
+            // Unitary gate. Check conditions.
+            if let Some(num_controls) = self.num_controls {
+                if num_controls != gate.get_controls().len() {
+                    // Mismatch in expected number of control qubits.
+                    return Ok(None);
+                }
+            }
+            if let Some(params) = self.matrix_converter.detect(&matrix)? {
+                // Matrix match; construct qubit argument vector.
+                let mut qubits = vec![];
+                qubits.extend(gate.get_controls().iter());
+                qubits.extend(gate.get_targets().iter());
+                // Construct data.
+                let mut data = gate.data.clone();
+                params.to_arb(&mut data);
+                Ok(Some((qubits, data)))
+            } else {
+                // Matrix didn't match.
+                Ok(None)
+            }
+        } else {
+            // A gate with no name, matrix, or measured qubits is illegal.
+            unreachable!();
+        }
+    }
+
+    fn construct(&self, output: &Self::Output) -> Result<Gate> {
+        let (qubits, data) = output;
+
+        // Construct the data.
+        let mut data = data.clone();
+        let params = M::Output::from_arb(&mut data)?;
+
+        // Construct the matrix.
+        let matrix = self.matrix_converter.construct(&params)?;
+
+        // Parse qubit argument vector.
+        let num_targets = matrix.num_qubits().unwrap();
+        let num_controls = qubits
+            .len()
+            .checked_sub(num_targets)
+            .ok_or_else(oe_inv_arg(format!("need at least {} qubits", num_targets)))?;
+        if let Some(expected) = self.num_controls {
+            if num_controls != expected {
+                inv_arg(format!(
+                    "expected {} control and {} target qubits",
+                    expected, num_targets
+                ))?;
+            }
+        }
+        let controls = &qubits[..num_controls];
+        let targets = &qubits[num_controls..];
+
+        // Construct the gate.
+        let mut gate =
+            Gate::new_unitary(targets.iter().cloned(), controls.iter().cloned(), matrix)?;
+        gate.data.copy_from(&data);
+
+        Ok(gate)
+    }
 }
 
 /// A type that can be used as a cache key in a Converter.
@@ -65,7 +185,7 @@ where
 {
     /// The collection of `Converter`s are stored in this map as trait objects
     /// with a wrapping tuple including the corresponding key.
-    converters: HashMap<K, Box<dyn Converter<I, O> + 'c>>,
+    converters: HashMap<K, Box<dyn Converter<Input = I, Output = O> + 'c>>,
     /// The order in which converters are called when
     order: Vec<K>,
     /// The cache is stored in a HashMap that maps from input type I to the
@@ -93,7 +213,11 @@ where
 
     /// Appends a Converter with the specified key to the back of the collection
     /// of Detectors in this map.
-    pub fn push(&mut self, key: impl Into<K>, converter: impl Converter<I, O> + 'c) {
+    pub fn push(
+        &mut self,
+        key: impl Into<K>,
+        converter: impl Converter<Input = I, Output = O> + 'c,
+    ) {
         let key: K = key.into();
         self.cache.borrow_mut().retain(|_, v| {
             if let Some((k, _)) = v {
@@ -119,7 +243,7 @@ where
         &mut self,
         index: usize,
         key: impl Into<K>,
-        converter: impl Converter<I, O> + 'c,
+        converter: impl Converter<Input = I, Output = O> + 'c,
     ) {
         self.clear_cache();
         let key: K = key.into();
@@ -135,7 +259,11 @@ where
 
     /// Appends the specified Converter with the corresponding specified key to
     /// the collection and returns the updated DetectorMap.
-    pub fn with(mut self, key: impl Into<K>, converter: impl Converter<I, O> + 'c) -> Self {
+    pub fn with(
+        mut self,
+        key: impl Into<K>,
+        converter: impl Converter<Input = I, Output = O> + 'c,
+    ) -> Self {
         self.push(key, converter);
         self
     }
@@ -156,13 +284,16 @@ where
     }
 }
 
-impl<'c, I, K, O, C> Converter<I, (K, O)> for ConverterMap<'c, K, I, O, C>
+impl<'c, I, K, O, C> Converter for ConverterMap<'c, K, I, O, C>
 where
     I: Clone,
     K: Eq + Hash + Clone,
     C: Eq + Hash + ConverterCacheKey<I>,
     O: Clone,
 {
+    type Input = I;
+    type Output = (K, O);
+
     fn detect(&self, input: &I) -> Result<Option<(K, O)>> {
         // Get the cache key for this input.
         let cache_key = C::from_input(input);
