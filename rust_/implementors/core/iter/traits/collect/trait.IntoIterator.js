7597a1e2fdee8d64666b88221f7446bdd944942b
mbrobbel <mbrobbel@users.noreply.github.com>
2020-03-30 08:07:03 +0000

    Deploy to GitHub Pages

--- rust_/implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ rust_/implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["dqcsim"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"dqcsim/common/types/struct.Matrix.html\" title=\"struct dqcsim::common::types::Matrix\">Matrix</a>","synthetic":false,"types":["dqcsim::core::common::types::matrix::Matrix"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rust_/implementors/core/ops/arith/trait.Sub.js
+++ rust_/implementors/core/ops/arith/trait.Sub.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["dqcsim"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i64.html\">i64</a>&gt; for <a class=\"struct\" href=\"dqcsim/common/types/struct.Cycle.html\" title=\"struct dqcsim::common::types::Cycle\">Cycle</a>","synthetic":false,"types":["dqcsim::core::common::types::cycle::Cycle"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"dqcsim/common/types/struct.Cycle.html\" title=\"struct dqcsim::common::types::Cycle\">Cycle</a>&gt; for <a class=\"struct\" href=\"dqcsim/common/types/struct.Cycle.html\" title=\"struct dqcsim::common::types::Cycle\">Cycle</a>","synthetic":false,"types":["dqcsim::core::common::types::cycle::Cycle"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
