cb49d9eec3a45fc2b02f3ea7dc420252301366e0
mbrobbel <mbrobbel@users.noreply.github.com>
2020-11-06 20:38:44 +0000

    deploy: 6b8f2818f4c93eebff4d469414cffe1efadd222c

--- rust_/implementors/core/iter/traits/collect/trait.IntoIterator.js
+++ rust_/implementors/core/iter/traits/collect/trait.IntoIterator.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["dqcsim"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"dqcsim/common/types/struct.Matrix.html\" title=\"struct dqcsim::common::types::Matrix\">Matrix</a>","synthetic":false,"types":["dqcsim::core::common::types::matrix::Matrix"]}];
+implementors["dqcsim"] = [{"text":"impl IntoIterator for Matrix","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
