8a44256f292273d4f9f4944c298e749b598b70ed
mbrobbel <mbrobbel@users.noreply.github.com>
2021-01-14 18:58:44 +0000

    deploy: 2257d1ec5dc9578929b1b0726d5ab299bcab04e9

---
+++ rust_/implementors/std/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["dqcsim"] = [{"text":"impl Error for NoLoglevel","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
